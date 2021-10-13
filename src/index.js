0fc4db116cc96dbd06c2df14ed18d3ed91f17278
rook16rus <kamalov00albert@gmail.com>
2021-10-13 20:00:48 +0300

    Стилизовал промо-блок и внедрил библиотека slick slider для слайдера

--- src/index.js
+++ src/index.js
@@ -1,2 +1,27 @@
-import './sass/normalize.css'
-import './sass/style.scss'
+import './sass/normalize.css';
+import './sass/style.scss';
+import '../libs/slick/slick.css'
+import '../libs/slick/slick.min'
+import $ from 'jquery';
+
+$(function(){
+    $('.top-slider').slick({
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+
+                }
+            },
+            {
+                breakpoint: 640,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
